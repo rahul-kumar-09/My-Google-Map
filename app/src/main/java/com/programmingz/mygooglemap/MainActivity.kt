916152a2712937d4a37c6c5ecc7491bdb47bcee1
Rahul kumar maury <rahu7827759@gmail.com>
2024-02-12 12:47:15 +0530

    Add map Add type of map

--- app/src/main/java/com/programmingz/mygooglemap/MainActivity.kt
+++ app/src/main/java/com/programmingz/mygooglemap/MainActivity.kt
@@ -2,6 +2,8 @@ package com.programmingz.mygooglemap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -16,6 +18,30 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+
+        val mapOptionButton: ImageButton = findViewById(R.id.mapOptionMenu)
+
+        val popupMenu = PopupMenu(this, mapOptionButton)
+        popupMenu.menuInflater.inflate(R.menu.map_options, popupMenu.menu)
+
+        popupMenu.setOnMenuItemClickListener { menuItem ->
+            changeMap(menuItem.itemId)
+            true
+        }
+        mapOptionButton.setOnClickListener {
+            popupMenu.show()
+        }
+
+    }
+
+    private fun changeMap(itemId: Int) {
+        when(itemId){
+            R.id.normal_map -> mGoogleMap?.mapType = GoogleMap.MAP_TYPE_NORMAL
+            R.id.hybrid_map -> mGoogleMap?.mapType = GoogleMap.MAP_TYPE_HYBRID
+            R.id.statelite_map -> mGoogleMap?.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            R.id.terrain_map -> mGoogleMap?.mapType = GoogleMap.MAP_TYPE_TERRAIN
+
+        }
     }
 
     override fun onMapReady(googlemap: GoogleMap) {
